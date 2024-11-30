25bda9d9121a236b677f6ccc1d9208d68995c864
mmmonk21 <101047972+mmmonk21@users.noreply.github.com>
2024-11-30 20:06:01 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from selenium import webdriver
+
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
